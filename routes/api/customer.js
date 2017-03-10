a8cf8d08fe62d61de6388243080d431d1341f903
Rui Li <liruiwh@cn.ibm.com>
2017-03-10 17:00:46 +0800

    add /customer/:id api

--- routes/api/customer.js
+++ routes/api/customer.js
@@ -5,5 +5,9 @@ router.get("/", function(req, res){
     res.send("this is customer list");
 });
 
+router.get("/:id", function(req, res){
+
+    res.send({id:req.params.id, name:'Li Rui'});
+});
 
 module.exports = router;
